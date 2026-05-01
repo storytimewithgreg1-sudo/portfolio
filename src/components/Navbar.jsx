2b293e9542d01b3775742ad8796a41609c47016a
Greg <gregworlanyo1@gmail.com>
2026-05-01 17:32:38 +0100

    adjusted button and progress bar size

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,12 +5,12 @@ const Navbar = () => {
     <div className='w-full '>
         <nav className='flex justify-around items-center max-w-10xl md:max-w-13xl lg:max-w-15xl mx-auto p-5 '>
         <div className='flex items-center space-x-5'>
-            <span className='font-medium text-base-content  text-lg md:text-xl lg:text-2xl text-center'>Greg's-Repo</span>
+            <span className='font-medium text-base-content  text-sm md:text-md lg:text-lg text-center'>Greg's-Repo</span>
            
         </div>
 
         <div>
-            <button className='btn btn-accent text-base-200 rounded-xl'>
+            <button className='btn-sm md:btn-md btn-accent text-base-200 rounded-xl'>
                 About me
             </button>
 
